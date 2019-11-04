af04df1a607371c56c8f632d74b157b11e275980
Dan Wilt <dwilt4rville@gmail.com>
2019-11-04 07:04:31 -0800

    moved to library

---
+++ src/lib/components/Example.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Example.css";
+
+const Example = () => (
+  <div className="Example">
+    <h1 className="Example-text">Create React Library</h1>
+  </div>
+);
+
+export default Example;
