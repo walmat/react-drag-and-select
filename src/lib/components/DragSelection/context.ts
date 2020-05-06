8580622321fa77da364ff8d7001bbad3167a039d
Dan Wilt <dwilt4rville@gmail.com>
2020-05-06 13:33:43 -0700

    Update context.ts

--- src/lib/components/DragSelection/context.ts
+++ src/lib/components/DragSelection/context.ts
@@ -1,8 +1,21 @@
 import { createContext } from "react";
 import { SelectionBox } from "./types";
 
+interface Item {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
 export const DragSelectionContext = createContext<{
   selectionBox: SelectionBox | null;
+  items: {
+    [key: string]: Item;
+  };
+  registerItem: (item: Item) => void;
 }>({
-  selectionBox: null
+  selectionBox: null,
+  items: {},
+  registerItem: (item) => {},
 });
